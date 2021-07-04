45491a1d0acf00b677d82817c7f4e113ee7517c4
larkes00 <zakirov2802@gmail.com>
2021-07-04 16:54:55 +0300

    Creating task and list models, and create migrations

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from treebeard.mp_tree import MP_Node
 
-# Create your models here.
+
+class List(models.Model):
+    name = models.CharField(max_length=128)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(MP_Node):
+    title = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    node_order_by = ['title']
+
+    def __str__(self):
+        return self.title
