34ccbe3e22fb10a28aba7b742fb11506991cf378
larkes00 <zakirov2802@gmail.com>
2021-07-04 16:55:29 +0300

    Registering models in the admin

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+
+from .models import *
+
 
 # Register your models here.
+
+class TaskAdmin(TreeAdmin):
+    form = movenodeform_factory(Task)
+
+
+admin.site.register(List)
+admin.site.register(Task, TaskAdmin)
