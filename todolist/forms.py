cb86d3e4d45cbc8918ac8e2a94c9cca66f14a725
larkes00 <zakirov2802@gmail.com>
2021-07-07 23:27:35 +0300

    The form for the task and the list have been changed

--- todolist/forms.py
+++ todolist/forms.py
@@ -3,10 +3,14 @@ from .models import *
 
 
 class AddTaskForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    list = forms.ModelChoiceField(queryset=List.objects.all())
+    title = forms.CharField(max_length=255, label="Задача")
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={"cols": 30, "rows": 10}),
+        label="Описание задачи",
+        required=False
+    )
+    list = forms.ModelChoiceField(queryset=List.objects.all(), label="Список", empty_label="Список не выбран")
 
 
 class AddListForm(forms.Form):
-    name = forms.CharField(max_length=128)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = forms.CharField(max_length=128, label="Название списка")
