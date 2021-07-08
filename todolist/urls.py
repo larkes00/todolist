054388d8f511bb6210a290f449896133c5dd6093
larkes00 <zakirov2802@gmail.com>
2021-07-08 22:45:33 +0300

    Added path

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("main/", task_list, name="home"),
-    path("task/", new_task, name="task_create")
+    path("task/", new_task, name="task_create"),
+    path("list/", new_list, name="list_create")
 ]
