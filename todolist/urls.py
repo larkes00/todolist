f1e15633441d4c101ce7a5170de78c2eeb12db4f
larkes00 <zakirov2802@gmail.com>
2021-07-09 19:57:10 +0300

    Registering and changing views

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("main/", task_list, name="home"),
-    path("task/", new_task, name="task_create"),
-    path("list/", new_list, name="list_create")
+    path("tasks/", TaskList.as_view(), name="home"),
+    path("lists/<int:pk>/", ShowList.as_view(), name="get_list"),
+    path("task/", NewTask.as_view(), name="task_create"),
+    path("register/", RegisterUser.as_view(), name="register"),
+    # path("login/", name="login")
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,33 +1,74 @@
-from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
-from todolist.forms import AddTaskForm, AddListForm
+from todolist.forms import AddTaskForm, RegisterUserForm
 from .models import *
 
 
-def task_list(request):
-    tasks = Task.objects.all()
-    return render(request, "todolist/index.html", {"tasks": tasks})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = "todolist/register.html"
+    success_url = reverse_lazy("login")
 
 
-def new_task(request):
-    if request.method == "POST":
-        form = AddTaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddTaskForm()
-
-    return render(request, "todolist/add_task.html", {"form": form})
+# class LoginUser(CreateView):
+#     template_name = "todolist/login.html"
 
 
-def new_list(request):
-    if request.method == "POST":
-        form = AddListForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddListForm()
+class TaskList(ListView):
+    model = Task
+    template_name = "todolist/index.html"
+    context_object_name = "tasks"
 
-    return render(request, "todolist/add_list.html", {"form": form})
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["menu"] = [{"x": 1}, {"x": 2}]
+        context["something"] = 'Information'
+        return context
+
+    def get_queryset(self):
+        return Task.objects.filter(completed=False)
+
+
+class ShowList(DetailView):
+    model = List
+    template_name = "todolist/task.html"
+    context_object_name = "tasks"
+
+
+class NewTask(CreateView):
+    form_class = AddTaskForm
+    template_name = "todolist/add_task.html"
+    success_url = reverse_lazy("home")
+
+# class ListList(ListView):
+#     model = List
+#     template_name = "todolist/index.html"
+#     context_object_name = "list"
+#
+#     def get_queryset(self):
+#         return List.objects.filter(name__slug=self.kwargs["list_slug"])
+
+
+# def new_task(request):
+#     if request.method == "POST":
+#         form = AddTaskForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddTaskForm()
+#
+#     return render(request, "todolist/add_task.html", {"form": form})
+#
+#
+# def new_list(request):
+#     if request.method == "POST":
+#         form = AddListForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddListForm()
+#
+#     return render(request, "todolist/add_list.html", {"form": form})
